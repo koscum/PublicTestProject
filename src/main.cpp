5d7a60f6cfea436ce2b10527fbee4c627c0c6e01
Marko Vinu <koscum@gmail.com>
2016-03-07 22:52:14 +0100

    Added basic menu for test project selection.

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,41 @@
 #include "multithreading/producerConsumer/Main.h"
 
 #include <iostream>
+#include <string>
 
 int main(int argc, char **argv)
 {
-	std::cout << "Hello world!" << '\n';
+	int selection = 0;
 
-	multithreading::producerConsumer::Main producerConsumerMain;
+	while (true)
+	{
+		std::cout << std::string(128, '\n');
 
-	return producerConsumerMain.main(argc, argv);
+		std::cout << "Hello! Welcome to Public Test Project." << '\n'
+		          << '\n'
+		          << "Currently available test projects:" << '\n'
+		          << '\t' << "1) Multithreading::ProducerConsumer" << '\n'
+		          << '\t' << "0) Exit" << '\n'
+		          << '\n'
+		          << "Your choice: ";
+
+		std::cin >> selection;
+		switch(selection)
+		{
+			case 1:
+			{
+				multithreading::producerConsumer::Main producerConsumerMain;
+
+				std::cout << "Multithreading::ProducerConsumer returned with exit code " << producerConsumerMain.main(argc, argv) << "." << '\n';
+			}
+			case 0:
+			{
+				return 0;
+			}
+			default:
+			{
+				continue;
+			}
+		}
+	}
 }
